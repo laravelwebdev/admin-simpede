56170f63f1e0d55db0b0b02b77e1591a396c0b0d
Laravelwebdev <83898440+laravelwebdev@users.noreply.github.com>
2024-09-16 20:48:58 +0800

    Update Field.php

--- src/Fields/Field.php
+++ src/Fields/Field.php
@@ -52,6 +52,13 @@ abstract class Field extends FieldElement implements JsonSerializable, Resolvabl
     public $attribute;
 
     /**
+     * Indicates whether the field should be saved on action relation.
+     *
+     * @var bool
+     */
+    public $saveOnActionRelation = true;
+
+    /**
      * The field's resolved value.
      *
      * @var mixed
@@ -384,6 +391,18 @@ abstract class Field extends FieldElement implements JsonSerializable, Resolvabl
     public function displayUsing(callable $displayCallback)
     {
         $this->displayCallback = $displayCallback;
+
+        return $this;
+    }
+    
+    /**
+     * Prevents the field from being saved when performing an action on a related model.
+     *
+     * @return $this
+     */
+    public function doNotSaveOnActionRelation()
+    {
+        $this->saveOnActionRelation = false;
 
         return $this;
     }
